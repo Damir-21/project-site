7c94a1f676f6784c38c163e1be9d6e31cd9773d7
Damir-21 <60772313+Damir-21@users.noreply.github.com>
2020-07-12 23:25:17 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.main__body').slick({
+  prevArrow: '<i class="fas fa-angle-left"></i>',
+  nextArrow: '<i class="fas fa-angle-right"></i>',
+});
+
+$('.product__body').slick({
+  slidesToShow: 4,
+  prevArrow: '<i class="fas fa-angle-left ar"></i>',
+  nextArrow: '<i class="fas fa-angle-right ar"></i>',
+});
